5f6648f006ba09d87b625fb78c587fa1a4a468e1
tonimatasdev <79017964+TonimatasDEV@users.noreply.github.com>
2026-01-28 18:32:27 +0100

    fix: Registration

--- src/main/java/dev/d4nilpzz/goldenDandelion/GoldenDandelion.java
+++ src/main/java/dev/d4nilpzz/goldenDandelion/GoldenDandelion.java
@@ -1,35 +1,20 @@
 package dev.d4nilpzz.goldenDandelion;
 
 import com.mojang.logging.LogUtils;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.renderer.ItemBlockRenderTypes;
-import net.minecraft.client.renderer.RenderType;
-import net.minecraft.network.chat.Component;
-import net.minecraft.server.level.ServerPlayer;
+import dev.d4nilpzz.goldenDandelion.item.GoldenDandelionItem;
 import net.minecraft.world.effect.MobEffects;
-import net.minecraft.world.entity.AgeableMob;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.CreativeModeTabs;
-import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.FlowerBlock;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.material.MapColor;
-import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.IEventBus;
-import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.ModContainer;
-import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.common.Mod;
-import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
 import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.neoforged.neoforge.common.NeoForge;
 import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
-import net.neoforged.neoforge.event.entity.player.PlayerInteractEvent;
-import net.neoforged.neoforge.event.server.ServerStartingEvent;
-import net.neoforged.neoforge.event.tick.ServerTickEvent;
 import net.neoforged.neoforge.registries.DeferredBlock;
 import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
@@ -45,17 +30,13 @@ public class GoldenDandelion {
     public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(MODID);
 
     public static final DeferredBlock<Block> GOLDEN_DANDELION =
-            BLOCKS.register(
-                    "golden_dandelion",
-                    () -> new FlowerBlock(
-                            MobEffects.LUCK,
-                            5,
-
-                    )
+            BLOCKS.register("golden_dandelion", () ->
+                    new FlowerBlock(MobEffects.LUCK, 5, BlockBehaviour.Properties.of().mapColor(MapColor.COLOR_YELLOW).noCollission()
+                            .instabreak().sound(SoundType.GRASS).offsetType(BlockBehaviour.OffsetType.XZ).noOcclusion())
             );
 
-    public static final DeferredItem<BlockItem> GOLDEN_DANDELION_ITEM =
-            ITEMS.registerSimpleBlockItem("golden_dandelion", GOLDEN_DANDELION);
+    public static final DeferredItem<BlockItem> GOLDEN_DANDELION_ITEM = ITEMS.registerItem("golden_dandelion", (properties) -> 
+            new GoldenDandelionItem(GOLDEN_DANDELION.get(), properties));
 
     public GoldenDandelion(IEventBus modEventBus, ModContainer modContainer) {
         BLOCKS.register(modEventBus);
@@ -63,8 +44,6 @@ public class GoldenDandelion {
 
         modEventBus.addListener(this::addCreative);
         modEventBus.addListener(this::commonSetup);
-
-        NeoForge.EVENT_BUS.register(this);
     }
 
     private void commonSetup(final FMLCommonSetupEvent event) {
@@ -74,24 +53,6 @@ public class GoldenDandelion {
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
         if (event.getTabKey() == CreativeModeTabs.FOOD_AND_DRINKS) {
             event.accept(GOLDEN_DANDELION_ITEM);
-        }
-    }
-
-    @SubscribeEvent
-    public void onServerStarting(ServerStartingEvent event) {
-        LOGGER.info("Server starting");
-    }
-
-    @EventBusSubscriber(modid = MODID, value = Dist.CLIENT)
-    public static class ClientModEvents {
-        @SubscribeEvent
-        public static void onClientSetup(FMLClientSetupEvent event) {
-            event.enqueueWork(() -> {
-                ItemBlockRenderTypes.setRenderLayer(
-                        GOLDEN_DANDELION.get(),
-                        RenderType.cutout()
-                );
-            });
         }
     }
 }
